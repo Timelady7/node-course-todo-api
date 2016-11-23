82e26a699985a7ec044c889dc34a5d3e33a5d834
Timelady7 <timelady47@gmail.com>
2016-11-23 17:58:35 +0100

    DELETE tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,7 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'First test todo'
 }];
 
 beforeEach((done) => {
@@ -97,4 +97,41 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should delete a todo', (done) => {
+        const hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        const hexId = new ObjectID().toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if non object id', (done) => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done);
+    });
 });
