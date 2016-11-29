90bdc39241c1d0a9d7ad2e452ffcada3177f73f3
Timelady7 <timelady47@gmail.com>
2016-11-29 10:21:21 +0100

    hash user pwd before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash (password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+var hashedPass = '$2a$10$3OhD12h2uBit/Dem0Xdbb.cgwrWqcSHmWd/mPW90pPdG/5AqZ749q';
+
+bcrypt.compare(password, hashedPass, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 
 // const message = 'I am user number 3';
